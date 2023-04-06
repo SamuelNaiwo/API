fffbef4ffdbdd6922760b974478382af449c74fb
Samuel Naiwo <samuelnaiwo@Samuels-MacBook-Pro.local>
2023-04-06 17:35:38 +0100

    api requests file

--- api_requests.py
+++ api_requests.py
@@ -11,13 +11,10 @@ print(post_codes_req.json())
 print(type(post_codes_req.content))
 print(type(post_codes_req.json()))
 
-pokemon_req = requests.get("https://p)
 
 # POST - Very API dependent.
 # json.dumps() - Turns a python object into a JSON string (converts)
-json_body = json.dumps({"postcodes": ["PR* 0SG", "M45 6GN", "EX165BL"]})
+json_body = json.dumps({"postcodes": ["PR1 0SG", "M45 6GN", "EX165BL"]})
 headers = {"Content-Type": "application/json"}
 post_multi_req = requests.post("https://api.postcodes.io/postcodes/", headers=headers, data=json_body)
 print(post_multi_req.json())
-
-pokemon
